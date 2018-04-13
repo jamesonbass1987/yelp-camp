4295053300f47ec7038e3ba26fef15d4f971a3e1
jamesonbass1987 <jamesonbass@gmail.com>
2018-04-13 10:17:07 -0700

    added bootstrap to project

--- app.js
+++ app.js
@@ -10,5 +10,12 @@ app.get('/', (req, res) => {
     res.render('landing')
 });
 
+app.get('/campgrounds', (req, res) => {
+    const campgrounds = [
+        { name: "Salmon Creek", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg" },{ name: "Granite Hill", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg" },
+        { name: "Mountain Goat Rest", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg" }];
+
+    res.render('campgrounds', { campgrounds } );
+});
 
 app.listen(3000);
